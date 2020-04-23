5c66179f43ef35ff2a19d5a26307edc7f9d28c31
Christopher Mogler <christophermogler@outlook.de>
2020-04-23 22:20:35 +0200

    DB-Tabellen Logik verändert / verbessert
    - Modul-Name muss jetzt eindeutig sein
    - SM_Customers_Change Created muss gesetzt werden

--- src/SM.Database/SQL/Tables/SM_Modules.sql
+++ src/SM.Database/SQL/Tables/SM_Modules.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE "admin"."SM_Modules" (
 	"Module_ID" UNIQUEIDENTIFIER NOT NULL DEFAULT "newid"(),
-	"Name" VARCHAR(255) NOT NULL,
+	"Name" VARCHAR(255) NOT NULL UNIQUE,
 	"Created" TIMESTAMP NOT NULL DEFAULT "now"(),
 	"Deleted" TIMESTAMP NULL,
 	"IsActive" BIT NOT NULL COMPUTE( case when "Deleted" is null then 1 else 0 end ),
