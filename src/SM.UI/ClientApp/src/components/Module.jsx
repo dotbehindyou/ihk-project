d9fd0e99288000450525616483c95d9ce04991a5
Christopher Mogler <christophermogler@outlook.de>
2020-04-26 00:36:21 +0200

    Einstellungen für UI eingestellt
    - CORS aktivieren für WebAPI
    - React-Packete aktuallisiert
    - Standard Objekte von create-React-app gelöscht

---
+++ src/SM.UI/ClientApp/src/components/Module.jsx
@@ -0,0 +1,30 @@
+﻿import React from 'react';
+
+
+
+class Module extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            error: null,
+            isLoaded: false,
+            items: []
+        }
+    }
+
+    componentDidMount() {
+        fetch('https://localhost:44376/api/v1/Module')
+            .then(res => res.json())
+            .then((result) => this.setState({
+                isLoaded: true,
+                items: result
+            }));
+    }
+
+    render() {
+        return <div> {this.state.items.map((x) => <p>{x.module_ID}</p>)} </div>;
+    }
+}
+
+
+export default Module;
