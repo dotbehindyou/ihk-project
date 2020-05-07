610a5c74ab071ab399b9f3cef3278fa26dffdcfe
Christopher Mogler <christophermogler@outlook.de>
2020-05-07 15:42:26 +0200

    Falsche Spalte in der Datenbank selektiert.
    Es wurde nicht die Config-Daten des Kunden selektiert, sonder die default Version

--- src/SM.Database/Managers/ModuleManager.cs
+++ src/SM.Database/Managers/ModuleManager.cs
@@ -64,7 +64,7 @@ namespace SM.Managers
         {
             List<Module> mods = new List<Module>();
 
-            foreach (var mod in Mapper.GetMany<Modules_Version_Config>("select mod.Module_ID, mod.Name as ModuleName, cus.Version, cus.Status, cof.Config_ID, cof.FileName as ConfigFileName, cof.Data as ConfigData, ver.Release_Date, ver.Validation_Token " +
+            foreach (var mod in Mapper.GetMany<Modules_Version_Config>("select mod.Module_ID, mod.Name as ModuleName, cus.Version, cus.Status, cof.Config_ID, cos.FileName as ConfigFileName, cus.Config as ConfigData, ver.Release_Date, ver.Validation_Token " +
                 "from SM_Customers_Modules as cus " +
                         "inner join SM_Modules as mod on cus.Module_ID = mod.Module_ID and mod.IsActive = 1 " +
                         "left join SM_Modules_Version as ver on ver.Module_ID = cus.Module_ID and ver.Version = cus.Version " +
@@ -95,11 +95,11 @@ namespace SM.Managers
         public List<Module> GetModulesForService(Int32 kdnr)
         {
             List<Module> mods = new List<Module>();
-            foreach(var mod in Mapper.GetMany<Modules_Version_Config>("select mod.Module_ID, mod.Name as ModuleName, cus.Version, cus.Status, cof.Config_ID, cof.FileName as ConfigFileName, cof.Data as ConfigData, ver.Release_Date, ver.Validation_Token from SM_Customers_Modules as cus " +
+            foreach(var mod in Mapper.GetMany<Modules_Version_Config>("select mod.Module_ID, mod.Name as ModuleName, cus.Version, cus.Status, cof.Config_ID, cof.FileName as ConfigFileName, cus.Config as ConfigData, ver.Release_Date, ver.Validation_Token from SM_Customers_Modules as cus " +
                     "left join SM_Modules as mod on cus.Module_ID = mod.Module_ID " +
                     "left join SM_Modules_Version as ver on ver.Module_ID = cus.Module_ID and ver.Version = cus.Version " +
                     "left join SM_Modules_Config as cof on cof.Module_ID = mod.Module_ID and cof.Config_ID = ver.Config_ID " +
-                    "where cus.Kdnr = ? and cus.IsActive = 1",
+                    "where cus.Kdnr = ? and cus.IsActive = 1", true,
                     new OdbcParameter("kdnr", kdnr)))
             {
                 mods.Add(new Module
