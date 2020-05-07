cf0136d7a918a2823f39c30f27900f0cd1538aff
Christopher Mogler <christophermogler@outlook.de>
2020-05-07 18:25:07 +0200

    SM_Modules_Installed Skript an Tabellenänderungen angepasst

--- src/SM.Database/SQL/Tables/SM_Modules_Installed.sql
+++ src/SM.Database/SQL/Tables/SM_Modules_Installed.sql
@@ -5,6 +5,10 @@
 	"ValidationToken" BINARY(64) NOT NULL,
 	"ModuleName" VARCHAR(255) NOT NULL,
 	"Path" VARCHAR(512) NOT NULL,
+	"Installed" TIMESTAMP NOT NULL DEFAULT "now"(),
+	"Updated" TIMESTAMP NULL,
+	"Deleted" TIMESTAMP NULL,
+	"IsActive" BIT NOT NULL COMPUTE( case when "Deleted" is null then 1 else 0 end ),
 	PRIMARY KEY ( "Module_ID" ASC )
 ) IN "system";
 COMMENT ON COLUMN "admin"."SM_Modules_Installed"."ServiceName" IS 'Dienstname beim Installieren';
@@ -12,3 +16,7 @@ COMMENT ON COLUMN "admin"."SM_Modules_Installed"."Version" IS 'installierte Vers
 COMMENT ON COLUMN "admin"."SM_Modules_Installed"."ValidationToken" IS 'aktueller Hash der Version';
 COMMENT ON COLUMN "admin"."SM_Modules_Installed"."ModuleName" IS 'aktueller Name des Moduls';
 COMMENT ON COLUMN "admin"."SM_Modules_Installed"."Path" IS 'Pfad wo es installiert ist';
+COMMENT ON COLUMN "admin"."SM_Modules_Installed"."Installed" IS 'Wann wurde der Dienst installiert';
+COMMENT ON COLUMN "admin"."SM_Modules_Installed"."Updated" IS 'Wann wurde es zuletzt aktualisiert';
+COMMENT ON COLUMN "admin"."SM_Modules_Installed"."Deleted" IS 'Wann wurde der Dienst gelöscht';
+COMMENT ON COLUMN "admin"."SM_Modules_Installed"."IsActive" IS 'Noch installiert?';
