d548f5718f7403736f49176b4d397e29dd633a68
Christopher Mogler <mogler@weissedv.de>
2020-05-11 12:35:57 +0200

    SM.Service ist jezt vollständig ein Dienst; Kann sich selbst installieren

--- src/SM.Service/Program.cs
+++ src/SM.Service/Program.cs
@@ -4,6 +4,7 @@ using System.Configuration;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.ServiceProcess;
 using System.Text;
 using SM.Managers;
@@ -14,92 +15,41 @@ using SM.Service.Models;
 
 namespace SM.Service
 {
-    static class Program
-    {
+    static class Program {
         /// <summary>
         /// Der Haupteinstiegspunkt für die Anwendung.
         /// </summary>
-        static void Main()
+        static void Main(String[] args)
         {
+
             if (Environment.UserInteractive)
             {
-                // TODO Install
-
                 Config.Current = new Config() { ConnectionString = ConfigurationManager.AppSettings["connectionString"] };
 
-                ApiController apiC = new ApiController();
-
-                List<Module> modules = apiC.GetModules();
-
-                List<SM_Modules_Installed> installedModules = null;
-                using (CustomerServiceManager sm = new CustomerServiceManager())
-                    installedModules = sm.GetMany();
-
-                //var t = modules.Join(installedModules, i => i.Module_ID, m => m.Module_ID, (m, i) => new { m, i });
-                var result = from mod in modules
-                             join ins in installedModules on mod.Module_ID equals ins.Module_ID into jmods
-                             from jmod in jmods.DefaultIfEmpty()
-                             select new
-                             {
-                                 Module = mod,
-                                 ServiceInformation = jmod,
-                                 IsInstalled = jmod != null
-                             };
-
-                ModuleController mc = new ModuleController();
-
-                foreach(var mod in result)
+                Models.Service SM_Service = new Models.Service
                 {
-                    Models.Service serv;
-                    if (mod.Module.Status == "DEL")
-                    {
-                        serv = mc.Remove(mod.Module);
-                        apiC.SendRemove(serv);
+                    Name = "ServiceManager_Weiss",
+                    Path = Assembly.GetExecutingAssembly().Location,
+                };
 
-                        continue;
-                    }
-                    
-                    if (mod.IsInstalled)
-                    {
-                        Byte[] file = null;
-                        SM_Modules_Installed sm = installedModules.Where(x=> x.Module_ID == mod.Module.Module_ID).FirstOrDefault();
+                if (args?.Any(x => x.ToLower() == "-i") ?? false) // -i als Parameter zum installieren
+                {
+                    Helper.ServiceInstaller.Install(SM_Service.Path, SM_Service.Name, "Service Manager");
+                }
+                else if (args?.Any(x => x.ToLower() == "-u") ?? false) // -u als Parameter zum deinstallieren
+                {
+                    Helper.ServiceInstaller.Uninstall(SM_Service.Name);
+                }
 
-                        if (sm.ValidationToken != mod.Module.Validation_Token)
-                        {
-                            file = apiC.GetFile(mod.Module);
-                        }
-
-                        try
-                        {
-                            serv = mc.Set(mod.Module, file);
-                            apiC.SendStatus(serv);
-                            continue;
-                        }
-                        catch (ServiceNotInstalledException e)
-                        {
-
-                        }
-                    }
-
-                    try
-                    {
-                        Byte[] file = apiC.GetFile(mod.Module);
-                        serv = mc.Add(mod.Module, file);
-                        apiC.SendStatus(serv);
-                    }
-                    catch (ServiceAlreadyInstalledException e)
-                    {
-
-                    }
+                if (args?.Any(x => x.ToLower() == "-d") ?? false) // -d zum Debuggen 
+                {
+                    // TODO Debuggen ServiceBase.Run(new ServiceManager());
                 }
             }
             else
             {
                 ServiceBase[] ServicesToRun;
-                ServicesToRun = new ServiceBase[]
-                {
-                new ServiceManager()
-                };
+                ServicesToRun = new ServiceBase[] { new ServiceManager() };
                 ServiceBase.Run(ServicesToRun);
             }
         }
--- src/SM.Service/ServiceManager.cs
+++ src/SM.Service/ServiceManager.cs
@@ -12,17 +12,18 @@ using System.Diagnostics;
 using System.Linq;
 using System.ServiceProcess;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Timers;
 
 namespace SM.Service
 {
-    public partial class ServiceManager : ServiceBase
-    {
-        private Timer updateStatus = new Timer(300 * 1000);
-        private Timer updateRessource = new Timer(30 * 1000 * 60);
+    public partial class ServiceManager : ServiceBase {
+        private readonly ApiController apiC = new ApiController();
+        private Thread serviceHanlderThread = null;
 
-        private ApiController apiC = new ApiController();
+        public CancellationTokenSource CancellationTokenSource { get; private set; } = new CancellationTokenSource();
+        public CancellationToken CancellationToken { get => CancellationTokenSource.Token; }
 
         public ServiceManager()
         {
@@ -33,121 +34,155 @@ namespace SM.Service
 
         protected override void OnStart(string[] args)
         {
-            updateStatus.Elapsed += this.OnUpdateStatus;
-            updateRessource.Elapsed += this.OnUpdateRessources;
+            Config.Current = new Config() { ConnectionString = ConfigurationManager.AppSettings["connectionString"] };
 
-            updateStatus.AutoReset = true;
-            updateRessource.AutoReset = true;
-
-            updateStatus.Start();
-            updateRessource.Start();
-        }
-
-        protected override void OnPause()
-        {
-            updateStatus.Stop();
-            updateRessource.Stop();
-
-            base.OnPause();
-        }
-
-        protected override void OnContinue()
-        {
-            updateStatus.Start();
-            updateRessource.Start();
-
-            base.OnContinue();
-        }
-
-        private void OnUpdateStatus(Object sender, ElapsedEventArgs eventArgss)
-        {
-            updateStatus.Stop();
-            try
-            {
-                List<SM_Modules_Installed> installedModules = null;
-                using (CustomerServiceManager sm = new CustomerServiceManager())
-                    installedModules = sm.GetMany();
-
-                ModuleController mc = new ModuleController();
-
-                foreach (var mo in installedModules)
-                {
-                    apiC.SendStatus(mc.GetService(mo));
-                }
-
-            }
-            catch(Exception e)
-            {
-
-            }
-            updateStatus.Start();
-        }
-        private void OnUpdateRessources(Object sender, ElapsedEventArgs eventArgs)
-        {
-            updateRessource.Stop();
-            try
-            {
-                List<Module> modules = apiC.GetModules();
-
-                List<SM_Modules_Installed> installedModules = null;
-                using (CustomerServiceManager sm = new CustomerServiceManager())
-                    installedModules = sm.GetMany();
-
-                ModuleController mc = new ModuleController();
-
-                for (int i = 0; i < modules.Count; ++i)
-                {
-                    var m = modules[i];
-                    try
-                    {
-                        if (m.Status == "INIT")
-                        {
-                            Byte[] file = apiC.GetFile(m);
-                            apiC.SendStatus(mc.Add(m, file));
-                        }
-                        else if (m.Status == "UPDATE")
-                        {
-                            Byte[] file = null;
-                            SM_Modules_Installed sm = installedModules.Where(x=> x.Module_ID == m.Module_ID).FirstOrDefault();
-                            if (sm.ValidationToken != m.Validation_Token)
-                            {
-                                file = apiC.GetFile(m);
-                            }
-
-                            apiC.SendStatus(mc.Set(m, file));
-                        }
-                        else if (m.Status == "DEL")
-                        {
-                            apiC.SendRemove(mc.Remove(m));
-                        }
-                    }
-                    catch (ServiceNotInstalledException e)
-                    {
-                        if (m.Status != "DEL")
-                        {
-                            m.Status = "INIT";
-                            --i;
-                        }
-                    }
-                    catch (ServiceAlreadyInstalledException e)
-                    {
-                        m.Status = "UPDATE";
-                    }
-                    catch (Exception e)
-                    {
-                        Console.WriteLine(e);
-                    }
-                }
-            }
-            catch (Exception e)
-            {
-
-            }
-            updateRessource.Start();
+            serviceHanlderThread = new Thread(this.ServiceHandler);
         }
 
         protected override void OnStop()
         {
+            if(serviceHanlderThread?.Join(3000) ?? false)
+                serviceHanlderThread.Abort();
+        }
+
+        private void ServiceHandler()
+        {
+            Thread moduleSyncThread, statusSyncThread;
+            ManualResetEvent moduleSyncThreadResetEvent = new ManualResetEvent(false),
+                statusSyncThreadResetEvent = new ManualResetEvent(false);
+
+            List<SM_Modules_Installed> installedModules = null;
+            using (CustomerServiceManager sm = new CustomerServiceManager())
+                installedModules = sm.GetMany();
+
+            moduleSyncThread = new Thread(() =>
+            {
+                while (!CancellationToken.IsCancellationRequested)
+                {
+                    this.ModuleSync(installedModules);
+                    Thread.Sleep(1800 * 1000); // Jede hable Stunde die Version / Validation_Token abrufen
+                }
+
+                moduleSyncThreadResetEvent.Set();
+            });
+
+            statusSyncThread = new Thread(() =>
+            {
+                while (!CancellationToken.IsCancellationRequested)
+                {
+                    this.StatusSync(installedModules);
+                    Thread.Sleep(60 * 1000); // Jede Minute den Status aktualisieren 
+                }
+
+                statusSyncThreadResetEvent.Set();
+            });
+
+            moduleSyncThread.Start();
+            statusSyncThread.Start();
+
+            WaitHandle[] waitHandles = new WaitHandle[]
+            {
+                moduleSyncThreadResetEvent,
+                statusSyncThreadResetEvent
+            };
+
+            WaitHandle.WaitAll(waitHandles);
+        }
+
+        private void ModuleSync(List<SM_Modules_Installed> installedModules)
+        {
+            ModuleController mc = new ModuleController();
+
+            List<Module> modules = apiC.GetModules();
+
+            //var t = modules.Join(installedModules, i => i.Module_ID, m => m.Module_ID, (m, i) => new { m, i });
+            var result = from mod in modules
+                         join ins in installedModules on mod.Module_ID equals ins.Module_ID into jmods
+                         from jmod in jmods.DefaultIfEmpty()
+                         select new
+                         {
+                             Module = mod,
+                             ServiceInformation = jmod,
+                             IsInstalled = jmod != null
+                         };
+
+
+            foreach (var mod in result)
+            {
+                Models.Service serv;
+                if (mod.Module.Status == "DEL")
+                {
+                    serv = mc.Remove(mod.Module);
+                    apiC.SendRemove(serv);
+
+                    continue;
+                }
+                else if (mod.Module.Status != "INIT" && mod.Module.Status != "UPDATE")
+                    continue;
+
+                if (mod.IsInstalled)
+                {
+                    Byte[] file = null;
+                    SM_Modules_Installed sm = installedModules.Where(x=> x.Module_ID == mod.Module.Module_ID).FirstOrDefault();
+
+                    if (sm.ValidationToken != mod.Module.Validation_Token)
+                    {
+                        file = apiC.GetFile(mod.Module);
+                    }
+
+                    try
+                    {
+                        serv = mc.Set(mod.Module, file);
+                        apiC.SendStatus(serv);
+                        continue;
+                    }
+                    catch (ServiceNotInstalledException e)
+                    {
+
+                    }
+                }
+
+                try
+                {
+                    Byte[] file = apiC.GetFile(mod.Module);
+                    serv = mc.Add(mod.Module, file);
+                    apiC.SendStatus(serv);
+                }
+                catch (ServiceAlreadyInstalledException e)
+                {
+
+                }
+            }
+        }
+
+        private void StatusSync(List<SM_Modules_Installed> installedModules)
+        {
+            foreach (var ins in installedModules)
+            {
+                try
+                {
+                    ServiceController sc = new ServiceController(ins.ServiceName);
+
+                    var ser = new Models.Service()
+                    {
+                        Status = sc.Status,
+                        Name = ins.ServiceName,
+                        Module = new Module
+                        {
+                            Module_ID = ins.Module_ID,
+                            Name = ins.ModuleName,
+                            Status = sc.Status.ToString(),
+                            Version = sc.ServiceName
+                        }
+                    };
+
+                    apiC.SendStatus(ser);
+                }
+                catch (Exception e)
+                {
+
+                }
+            }
         }
     }
 }
