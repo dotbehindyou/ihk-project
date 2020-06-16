de801f9bdb4ba6a1df06600e69c0edb4c0833298
Christopher Mogler <christophermogler@outlook.de>
2020-06-16 12:49:26 +0200

    Update uuid.js

--- src/SM.UI/ClientApp/src/helper/uuid.js
+++ src/SM.UI/ClientApp/src/helper/uuid.js
@@ -3,7 +3,9 @@
 export function uuidv4() {
   return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
     var r = (Math.random() * 16) | 0,
-      v = c == "x" ? r : (r & 0x3) | 0x8;
+      v = c === "x" ? r : (r & 0x3) | 0x8;
     return v.toString(16);
   });
 }
+
+export const EmptyUUID = "00000000-0000-0000-0000-000000000000";
