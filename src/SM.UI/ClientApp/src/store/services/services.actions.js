73109764828d72dafd0858cc8162c349d4d848b1
Christopher Mogler <christophermogler@outlook.de>
2020-06-16 12:50:28 +0200

    Angepasst an neue API

--- src/SM.UI/ClientApp/src/store/services/services.actions.js
+++ src/SM.UI/ClientApp/src/store/services/services.actions.js
@@ -1,33 +1,88 @@
 import { API_Service } from "../__api";
 
 export const REQUEST_SERVICE = "REQUEST_SERVICES";
+
 function requestService() {
-  return {
-    type: REQUEST_SERVICE,
-  };
+    return {
+        type: REQUEST_SERVICE,
+    };
 }
+
 export const RECEIVE_SERVICE = "RECEIVE_SERVICES";
+
 function receiveService(serviceList) {
-  return {
-    type: RECEIVE_SERVICE,
-    data: serviceList,
-  };
+    return {
+        type: RECEIVE_SERVICE,
+        data: serviceList,
+    };
 }
+
 export const SELECT_SERVICE = "SELECT_SERVICE";
-export function selectService(index = null, service = null) {
-  return {
-    type: SELECT_SERVICE,
-    index,
-    service,
-  };
+export function selectService(service = null) {
+    return {
+        type: SELECT_SERVICE,
+        service,
+    };
+}
+
+export const CANCEL_VIEW_SERVICE = "CANCEL_VIEW_SERVICE";
+export function cancelViewService() {
+    return {
+        type: CANCEL_VIEW_SERVICE,
+    }
+}
+
+export const ADD_SERVICE = "ADD_SERVICE";
+export function addService() {
+    return {
+        type: ADD_SERVICE
+    }
+}
+
+export const EDIT_SERVICE = "EDIT_SERVICE";
+export function editService(service) {
+    return {
+        type: EDIT_SERVICE,
+        service
+    }
+}
+
+export const CANCEL_EDIT_SERVICE = "CANCEL_EDIT_SERVICE";
+export function cancelEditService(service) {
+    return {
+        type: CANCEL_EDIT_SERVICE,
+        service
+    }
+}
+
+// TODO Speicher Async
+export const SAVE_SERVICE = "SAVE_SERVICE";
+export function saveServiceAsync(service) {
+    return (dispatch) => {
+        dispatch(requestService());
+        return API_Service.create()
+            .then((res) => res.json())
+            .then((json) => dispatch(receiveService(json)));
+    };
+}
+
+// TODO Delete Async
+export const DELETE_SERVICE = "DELETE_SERVICE";
+export function deleteServiceAsync(serviceId) {
+    return (dispatch) => {
+        dispatch(requestService());
+        return API_Service.create()
+            .then((res) => res.json())
+            .then((json) => dispatch(receiveService(json)));
+    };
 }
 
 /** Lade die Daten von der API  */
 export function fetchAllServiceAsync() {
-  return (dispatch) => {
-    dispatch(requestService());
-    return API_Service.all()
-      .then((res) => res.json())
-      .then((json) => dispatch(receiveService(json)));
-  };
+    return (dispatch) => {
+        dispatch(requestService());
+        return API_Service.all()
+            .then((res) => res.json())
+            .then((json) => dispatch(receiveService(json)));
+    };
 }
--- src/SM.UI/ClientApp/src/store/services/services.reducer.js
+++ src/SM.UI/ClientApp/src/store/services/services.reducer.js
@@ -2,46 +2,45 @@ import {
   REQUEST_SERVICE,
   RECEIVE_SERVICE,
   SELECT_SERVICE,
+  ADD_SERVICE,
+  CANCEL_EDIT_SERVICE,
+  CANCEL_VIEW_SERVICE,
 } from "./services.actions";
+import { EmptyUUID } from "../../helper/uuid";
 
 const initialState = {
   services: [],
   isFetching: true,
   error: null,
+  isEditing: false,
   selected: {
-    index: 0,
+    index: -1,
     service: null,
   },
 };
 
-export default function servicesReducer(state = initialState, action) {
-  switch (action.type) {
-    case REQUEST_SERVICE:
-      return { ...state, isFetching: true };
-    case RECEIVE_SERVICE:
-      return { ...state, services: action.data, isFetching: false };
-    case SELECT_SERVICE:
-      return __select_service;
-    default:
-      return state;
+function __get_new_service(){
+  return {
+    module_ID: EmptyUUID,
+    name: '',
+    isNew: true
+  }
+}
+
+function __get_index(state, service){
+  for(var i = 0; i < state.services.length; ++i){
+    let x = state.services[i];
+    if (x.module_ID === service.module_ID) {
+      return i;
+    }
   }
 }
 
 function __select_service(state, action) {
-  if (action.index == null && action.service == null) return state;
+  if (action.service == null) return state;
 
-  let service, index;
-
-  if (action.index != null) {
-    service = state.services[index];
-  } else {
-    state.services.forEach((x, key) => {
-      if (x.module_ID === action.service.module_ID) {
-        service = x;
-        index = key;
-      }
-    });
-  }
+  let service = action.service, 
+    index = __get_index(state, action.service);
 
   return {
     ...state,
@@ -50,4 +49,38 @@ function __select_service(state, action) {
       service,
     },
   };
+}
+
+function __cancel_edit_service(state, action){
+  let serList, service = action.service;
+  if(service.isNew === true){
+    serList = state.services.filter(x=> x.module_ID !== service.module_ID);
+  }
+  else{
+    serList = state.services.map(x=> { x.isEditing = false; return x; });
+  }
+  return {
+    ...state,
+    services: serList,
+    isEditing: false
+  }
+}
+
+export default function servicesReducer(state = initialState, action) {
+  switch (action.type) {
+    case REQUEST_SERVICE:
+      return { ...state, isFetching: true };
+    case RECEIVE_SERVICE:
+      return { ...state, services: action.data, isFetching: false };
+    case SELECT_SERVICE:
+      return __select_service(state, action);
+    case ADD_SERVICE:
+      return { ...state, services:[...state.services, __get_new_service()],isEditing: true };
+    case CANCEL_EDIT_SERVICE:
+      return __cancel_edit_service(state, action);
+    case CANCEL_VIEW_SERVICE:
+      return { ...state, selected: { index: -1, service: null } };
+    default:
+      return state;
+  }
 }
